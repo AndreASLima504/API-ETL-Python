33bf0f9ad6d5f647fb5ac503bb435e6dc3be5b20
André <andre.aslima@hotmail.com>
2023-10-28 00:30:20 -0300

    CRUD terminado, só falta o DELETE, adequar alguns dados do BD01, gerar logs e consumir API

---
+++ Conexoes.py
@@ -0,0 +1,95 @@
+import sqlite3 as sql
+
+bancoOrigem = "Banco1"
+BancoDestino = "Banco2"
+
+class Banco:
+    def __init__(self, rotaBanco, tabela):
+        self.tabela = tabela
+        self.database = rotaBanco
+        self.conn = None
+        self.cur = None
+        self.connected = False
+
+    def connect(self):
+        self.conn = sql.connect(self.database)
+        self.cur = self.conn.cursor()
+        self.connected = True
+
+    def disconnect(self):
+        self.conn.close()
+        self.connected = False
+
+    def rollback(self):
+        if self.connected:
+            self.conn.rollback()
+
+    def execute(self, sql, parms=None):
+        if self.connected:
+            if parms is None:
+                self.cur.execute(sql)
+            else:
+                self.cur.execute(sql, parms)
+            return True
+        else:
+            return False
+
+    def fetchall(self):
+        return self.cur.fetchall()
+
+    def persist(self):
+        if self.connected:
+            self.conn.commit()
+            return True
+        else:
+            return False
+    
+    def initDB(self):
+        self.connect()
+        # self.execute("CREATE TABLE IF NOT EXISTS Client (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT (50), surname TEXT(50), email TEXT(320), cpf CHAR(11) CHECK(length(cpf) = 11))")
+        self.persist()
+        self.disconnect()
+
+    def insert(self, Nome, RG, CPF, Data_admissao, Data_hora_alteracao_do_registro, CEP, endereco, bairro, cidade):
+        self.connect()
+        try:
+            self.execute(f"INSERT INTO {self.tabela} VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (Nome, RG, CPF, Data_admissao, Data_hora_alteracao_do_registro, CEP, endereco, bairro, cidade))
+            self.persist()
+            self.disconnect()
+        except sql.IntegrityError as e:
+            self.rollback()
+
+    def read(self): 
+        self.connect()
+        self.execute(f"SELECT * FROM {self.tabela};")
+        rows = self.fetchall()
+        self.disconnect()
+        return rows
+
+    def search(self, id):
+        self.connect()
+        self.execute(f"SELECT * FROM {self.tabela} WHERE ID = ?", (id))
+        rows = self.fetchall()
+        self.disconnect()
+        return rows
+
+    def delete(self, id, ):
+        self.connect()
+        try:
+            self.execute(f"DELETE FROM {self.tabela} WHERE ID = ?", (id, ))
+            self.persist()
+            self.disconnect()
+            return "success"
+        except:
+            self.rollback()
+            return "Error"
+
+    def update(self, id, Nome, RG, CPF, Data_admissao, Data_hora_alteracao_do_registro, CEP, endereco, bairro, cidade):
+        self.connect()
+        self.execute(f"UPDATE {self.tabela} SET Nome = ?, RG = ?, CPF = ?, Data_admissao = ?, Data_hora_alteracao_do_registro = ?, CEP = ?, endereco = ?, bairro = ?, cidade = ? WHERE ID = ?", (Nome, RG, CPF, Data_admissao, Data_hora_alteracao_do_registro, CEP, endereco, bairro, cidade, id))
+        self.persist()
+        self.disconnect()
+
+
+
+
---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify
+import Conexoes as Cons
+from datetime import datetime
+import requests
+import json
+
+
+
+BancoOrigem = Cons.Banco("Banco1", "funcionarios")
+BancoDestino = Cons.Banco("Banco2", "funcionarios_fabrica")
+
+app = Flask(__name__)
+
+@app.route('/copiar_dados', methods=['GET'])
+def copy_data():
+    dadosOrigem = get_origem()
+    for i in dadosOrigem:
+        _enderecoCompleto = requests.get(f'https://viacep.com.br/ws/{i[6]}/json/')
+        enderecoCompleto = json.loads(_enderecoCompleto)
+        
+        dadosInsert = {
+            "ID": i[0],
+            "Nome": i[1],
+            "RG": i[2],
+            "CPF": i[3],
+            "Data_admissao": i[4],
+            "Data_alteracao_do_registro": datetime.now(),
+            "CEP": i[6],
+            "endereco": enderecoCompleto['logradouro'],
+            "bairro": enderecoCompleto['bairro'],
+            "cidade": enderecoCompleto['localidade']
+        }
+        # BancoDestino.insert[dadosInsert['ID'], dadosInsert['Nome'], dadosInsert['RG'], dadosInsert['CPF'], dadosInsert['Data_admissao'], dadosInsert['Data_alteracao_do_registro'], dadosInsert['CEP'], dadosInsert['endereco'], dadosInsert['bairro'], dadosInsert['cidade']]
+    return 
+
+
+@app.route('/dados_origem', methods=['GET'])
+def get_origem():
+    dadosOrigem = BancoOrigem.read()
+    return dadosOrigem
+
+
+@app.route('/dados_destino', methods=['GET'])
+def get_destino():
+    dadosDestino = BancoDestino.read()
+    return dadosDestino
+
+@app.route('/dados_destino/<id>', methods=['PUT'])
+def update_target_data(id):
+    BancoOrigem.update(id, )
+    return BancoOrigem.search(id)
+
+
+@app.route('/target_data/<id>', methods=['DELETE'])
+def delete_target_data(id):
+    BancoDestino.delete(id)
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
