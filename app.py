f7cc6da7cc3c2d8273a0863a1cff91566fb5f826
André <andre.aslima@hotmail.com>
2023-10-28 01:18:04 -0300

    Registros do banco1 regularizados e função de copiar banco funcionando

--- app.py
+++ app.py
@@ -11,27 +11,31 @@ BancoDestino = Cons.Banco("Banco2", "funcionarios_fabrica")
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def teste():
+    data = datetime.now().strftime('Hora: %H:%M:%S  Data: %d-%m-%Y')
+    return data
+
 @app.route('/copiar_dados', methods=['GET'])
 def copy_data():
     dadosOrigem = get_origem()
     for i in dadosOrigem:
-        _enderecoCompleto = requests.get(f'https://viacep.com.br/ws/{i[6]}/json/')
-        enderecoCompleto = json.loads(_enderecoCompleto)
-        
+        enderecoCompleto = requests.get(f'https://viacep.com.br/ws/{i[6]}/json/').json()
         dadosInsert = {
             "ID": i[0],
             "Nome": i[1],
             "RG": i[2],
             "CPF": i[3],
             "Data_admissao": i[4],
-            "Data_alteracao_do_registro": datetime.now(),
+            "Data_alteracao_do_registro": datetime.now().strftime('Hora: %H:%M:%S  Data: %d-%m-%Y'),
             "CEP": i[6],
             "endereco": enderecoCompleto['logradouro'],
             "bairro": enderecoCompleto['bairro'],
             "cidade": enderecoCompleto['localidade']
         }
+        print(dadosInsert)
         # BancoDestino.insert[dadosInsert['ID'], dadosInsert['Nome'], dadosInsert['RG'], dadosInsert['CPF'], dadosInsert['Data_admissao'], dadosInsert['Data_alteracao_do_registro'], dadosInsert['CEP'], dadosInsert['endereco'], dadosInsert['bairro'], dadosInsert['cidade']]
-    return 
+    return 'hello world'
 
 
 @app.route('/dados_origem', methods=['GET'])
